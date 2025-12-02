478a3e2228a52a64199c29bbbab4fb30bbb810d9
sazzzzan10 <sazzzzan10@gmail.com>
2025-12-03 02:00:38 +0545

    complete topbar

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from 'next/link';
 import React, { useState, useEffect } from 'react';
-
+import { HiBars3 } from 'react-icons/hi2';
 const Header = ()=>{
     const [isScrolled, setIsScrolled]=useState(false);
     const [isOpen, setIsOpen] = useState(false);
@@ -15,18 +15,35 @@ const Header = ()=>{
         }
     },[])
     return (
-        <header className={`fixed top-0 left-0 right-0 z-50 transition-colors duration-300 ${isScrolled } || isOpen ? 'bg-[#0b0416]/70 background-blur-md shadow-[0_0_40px_rgba(113, 39,186,0.20)]':'bg-transparent'}`}>
+        <header className={`fixed top-0 left-0 right-0 z-999 transition-colors duration-300 ${isScrolled  || isOpen ? 'bg-[#0b0416]/80 background-blur-md shadow-[0_0_40px_rgba(113, 39,186,0.20)]':'bg-transparent'}`}>
             <div className="max-w-7xl mx-auto px-6 lg:px-8 py-4 flex items-center justify-between">
             <Link href="#" className="text-3xl font-extrabold text-[#7e22ce] flex-1">E</Link>
-            <nav className="md:flex justify-around space-x-10 font-medium text-lg text-white/80">
+            <nav className="hidden md:flex space-x-10 font-medium text-lg text-white/80">
                 <Link href="#home" className="hover:text-[#a855f7] transition duration-200">Home</Link>
-                <Link href="#home" className="hover:text-[#a855f7] transition duration-200">About</Link>
-                <Link href="#home" className="hover:text-[#a855f7] transition duration-200">Projects</Link>
-                <Link href="#home" className="hover:text-[#a855f7] transition duration-200">Experience</Link> 
-                <Link href="#home" className="hover:text-[#a855f7] transition duration-200">Contact</Link> 
+                <Link href="#about" className="hover:text-[#a855f7] transition duration-200">About</Link>
+                <Link href="#projects" className="hover:text-[#a855f7] transition duration-200">Projects</Link>
+                <Link href="#experience" className="hover:text-[#a855f7] transition duration-200">Experience</Link> 
+                <Link href="#contact" className="hover:text-[#a855f7] transition duration-200">Contact</Link> 
 
             </nav>
+            <button onClick={()=>{setIsOpen(!isOpen)}} className="md:hidden p-2 rounded text-white/80 hover:text-[#a855f7] transition duration-200">
+                <HiBars3 className="size-6"/>
+            </button>
             </div>
+            {
+                isOpen && (
+                    <div className="md:hidden px-6 py-4 ">
+                                            <div className="flex flex-col gap-3 text-base font-medium text-white/80 ">
+
+                                        <Link href="#home" className="hover:text-[#a855f7] transition duration-200" onClick={()=>setIsOpen(false)}>Home</Link>
+                <Link href="#about" className="hover:text-[#a855f7] transition duration-200" onClick={()=>setIsOpen(false)}>About</Link>
+                <Link href="#projects" className="hover:text-[#a855f7] transition duration-200" onClick={()=>setIsOpen(false)}>Projects</Link>
+                <Link href="#experience" className="hover:text-[#a855f7] transition duration-200" onClick={()=>setIsOpen(false)}>Experience</Link> 
+                <Link href="#contact" className="hover:text-[#a855f7] transition duration-200" onClick={()=>setIsOpen(false)}>Contact</Link> 
+                        </div>
+                    </div>
+                )
+            }
         </header>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,16 @@ import ParallaxPage from "./parallax";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <>
+    <main className="max-w-7xl mx-auto px-6 lg:px-8">
+    {/* <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black"> */}
       {/* <ParallaxPage/> */}
-    </div>
+      <div id="home" className="min-h-screen py-24">Hero</div>
+      <div id="about" className="min-h-screen py-24">About</div>
+      <div id="projects" className="min-h-screen py-24">Projects</div>
+      <div id="experience" className="min-h-screen py-24">Experience</div>
+      <div id="contact" className="min-h-screen py-24">Contact</div>
+    </main>
+    </>
   );
 }
