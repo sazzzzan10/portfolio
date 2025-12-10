fd7366e2765342fed521bdfeab4e386f94b38d60
sazzzzan10 <sazzzzan10@gmail.com>
2025-12-10 22:13:27 +0545

    minor ui changes

--- app/components/ProjectItem.tsx
+++ app/components/ProjectItem.tsx
@@ -6,18 +6,19 @@ interface ProjectItemProps {
     name: string,
     description: string,
     position:number,
-    image:string
+    image:string,
+    scrollable: boolean
 }
 
-const ProjectItem = ({name, description, position, image}:ProjectItemProps)=>{
+const ProjectItem = ({name, description, position, image, scrollable}:ProjectItemProps)=>{
     return (
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-16 items-center ">
                 <div className={position%2==0?"flex flex-col md:order-1":"flex flex-col"}>
-                <h1 className={position%2!==0?"text-2xl sm:text-2xl font-normal tracking-light text-[#880000] leading-[1.15]":"text-2xl sm:text-2xl text-[#880000]  md:text-right font-normal tracking-light leading-[1.15]"}>
+                <h1 className={position%2!==0?"text-2xl sm:text-2xl font-normal tracking-light  leading-[1.15]":"text-2xl sm:text-2xl   md:text-right font-normal tracking-light leading-[1.15]"}>
                 {name}
                 </h1>
                 
-                <div className={position%2!==0?"relative z-100  w-[100%] op-0 md:w-[135%] z-100 bg-gray-800/60 p-4 mt-2 rounded-lg":"relative z-100  top-0 md:-left-[35%] md:w-[135%] z-100 bg-gray-800/60 p-4 mt-2 rounded-lg"}>
+                <div className={position%2!==0?"relative z-100 bg-linear-to-r from-[#800000]/30 vai-[#6d28d9]/30 to-transparent w-[100%] op-0 md:w-[135%] z-100 bg-gray-800/60 p-4 mt-2 rounded-lg":"relative z-100 bg-linear-to-r from-transparent vai-[#6d28d9]/30 to-[#800000]/30  top-0 md:-left-[35%] md:w-[135%] z-100 bg-gray-800/60 p-4 mt-2 rounded-lg"}>
 {description}               </div>
                 </div>
                 <ScrollReveal stagger={0.2} className='relative p-0'>
@@ -29,7 +30,7 @@ const ProjectItem = ({name, description, position, image}:ProjectItemProps)=>{
 
                         <div className="overflow-hidden h-[320px]" >
                                                               
-                            <Image src={image} alt="abc"  width={582} height={320} className="w-full h-auto duration-3000 ease-linear group-hover:-translate-y-[55%] group-active:-translate-y-[55%] group-focus:-translate-y-[55%] "        style={{objectFit:"contain"}}/>
+                            <Image src={image} alt="abc"  width={582} height={320} className={scrollable?"w-full h-auto duration-3000 ease-linear group-hover:-translate-y-[55%] group-active:-translate-y-[55%] group-focus:-translate-y-[55%] ":"w-full h-auto duration-1000 ease-linear group-hover:-translate-y-[10%] group-active:-translate-y-[10%] group-focus:-translate-y-[10%] "}        style={{objectFit:"contain"}}/>
 
                         </div>
                     </div>
--- app/util/data.tsx
+++ app/util/data.tsx
@@ -90,32 +90,43 @@ const projectsList =[
     {
         name: "Pressure Vessel 3D",
         description: "A 3D modeling, cloud software to design code‑compliant pressure vessel for oil/gas industries using Three.js. Also, Ddployed the dockerized react js apps using CI/CD pipeline to App Engine in Google Cloud",
-        image:"/images/projects/vessel.png"
+        image:"/images/projects/vessel.png",
+        scrollable:false
     },
     {
         name: "Apex Education Web",
         description: "Ed‑Tech web platform for educational academy to run online class and take online tests to help in remote study",
-        image:"/images/projects/apextest2.png"
+        image:"/images/projects/apextest2.png",
+        scrollable:false
+
     },
     {
         name: "Apex Education Mobile",
         description: "Ed‑Tech mobile app for educational academy to run online class and take online tests to help in remote study",
-        image:"/images/projects/apexmb.jpg"
+        image:"/images/projects/apexmb.jpg",
+        scrollable:true
+
     },
         {
         name: "RONB",
         description: "A react native app with more than 50K downloads for news delivery, blood donation, yellow pages for android and iOS",
-        image:"/images/projects/ronbmb.png"
+        image:"/images/projects/ronbmb.png",
+        scrollable:true,
+
     },
     {
         name: "Hive",
         description: "A real‑time web‑based drone management system to control and monitor a network of drones using React and Material UI",
-        image:"/images/projects/hive2.png"
+        image:"/images/projects/hive2.png",
+        scrollable:false
+
     },
     {
         name: "IMS",
         description: "A react native based mobile app to keep track of medicines in hospital inventory",
-        image:"/images/projects/ims2.png"
+        image:"/images/projects/ims2.png",
+        scrollable:false
+
     }
 ]
 export  {skillItems, projectsList};
