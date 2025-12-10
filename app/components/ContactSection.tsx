0361b71e1e77b532aadbddab688b40b3e3d3a5d4
sazzzzan10 <sazzzzan10@gmail.com>
2025-12-10 18:54:36 +0545

    complete cotact section

--- app/components/ContactSection.tsx
+++ app/components/ContactSection.tsx
@@ -36,14 +36,15 @@ const ContactSection = ()=>{
     return (
         <section id="contact" className="py-2 pb-26">
         <ScrollReveal stagger={0.15} className="">
-            <div className="flex gap-12 justify-between text-white-200">
-            <div>
-            <div>
-                <h3 className="text-3xl md:text-3xl font-semibold text-white/90 mb-7">
+                      <h3 className="text-3xl md:text-3xl font-semibold text-white/90 mb-7">
                 Contact</h3>
-                <p className="text-md text-white/60 max-w-xl">I am always eager to contribute to new projects. You can contact me at </p>
+            <div className="flex md:flex-row flex-col gap-12 justify-between text-white-200 order-1">
+            <div className="flex-4 order-2 md:order-1">
+            <div>
+    
+                <p className="text-md text-white/60 max-w-xl md:mt-4">I am always eager to contribute to new projects. You can contact me at </p>
                 <div className="mt-2 text-white/80">
-                <Link href="mailto:sajanamatya10@gmail.com" className="hover:text-[#a855f7]">sajanamatya10@gmail.com</Link>
+                <Link href="mailto:sajanamatya10@gmail.com" className="hover:text-[#a855f7] mb-4">sajanamatya10[at]gmail[dot]com</Link>
                 </div>
             </div>
             <div className="mt-6 flex items-center gap-4 text-white/80">
@@ -55,12 +56,12 @@ const ContactSection = ()=>{
             </Link>
             </div>
             </div>
-            <div className="">
+            <div className="flex-1 order-1 md:order-2">
             <form action={action} className=" ">
-                <input name="fullName" placeholder="Full Name" className="bg-gray-800 p-2 rounded-lg m-2" required/>
-                <input type="email" name="email" placeholder="Email" className="bg-gray-800 p-2 rounded-lg  m-2" required/>
-                <textarea placeholder="Details..." name="data" className="bg-gray-800 p-2 rounded-lg  m-2" rows={4} cols={45} required/><br></br>
-                      <button type="submit"  className="hover:cursor-pointer bg-gray-800 p-2 rounded-lg  m-2" disabled={isPending}>
+                <input name="fullName" placeholder="Full Name" className="bg-gray-800 p-2 rounded-lg md:m-2 my-2 mt-4 mr-2" required/>
+                <input type="email" name="email" placeholder="Email" className="bg-gray-800 p-2 rounded-lg  md:m-2 my-2" required/>
+                <textarea placeholder="Details..." name="data" className="bg-gray-800 p-2 rounded-lg  md:m-2 my-2" rows={4} cols={45} required/><br></br>
+                      <button type="submit"  className="hover:cursor-pointer bg-gray-800 p-2 rounded-lg  md:m-2 my-2" disabled={isPending}>
         {isPending ? "Submitting..." : "Submit"}
       </button>
       {
@@ -72,6 +73,7 @@ const ContactSection = ()=>{
             </form>
             </div>
             </div>
+
         </ScrollReveal>
         </section>
     )
