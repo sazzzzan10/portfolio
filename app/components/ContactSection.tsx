6656ed1c6c7dfa1708de32d9e5af2495e67928ca
sazzzzan10 <sazzzzan10@gmail.com>
2025-12-11 18:32:46 +0545

    minor ui adjustments

--- app/components/ContactSection.tsx
+++ app/components/ContactSection.tsx
@@ -40,7 +40,7 @@ const ContactSection = ()=>{
                       <h3 className="text-3xl md:text-3xl font-semibold text-white/90 mb-7">
                 Contact</h3>
             <div className="flex md:flex-row flex-col gap-12 justify-between text-white-200 order-1">
-            <div className="flex-4 order-2 md:order-1">
+            <div className="order-2 md:order-1">
             <div>
     
                 <p className="text-md text-white/60 max-w-xl md:mt-4">I am always eager to contribute to new projects. You can contact me at </p>
@@ -57,11 +57,11 @@ const ContactSection = ()=>{
             </Link>
             </div>
             </div>
-            <div className="flex-1 order-1 md:order-2">
+            <div className=" order-1 md:order-2">
             <form action={action} className=" ">
                 <input name="fullName" placeholder="Full Name" className="bg-gray-800 p-2 rounded-lg md:m-2 my-2 mt-4 mr-2" required/>
                 <input type="email" name="email" placeholder="Email" className="bg-gray-800 p-2 rounded-lg  md:m-2 my-2" required/>
-                <textarea placeholder="Details..." name="data" className="bg-gray-800 p-2 rounded-lg  md:m-2 my-2" rows={4} cols={45} required/><br></br>
+                <textarea placeholder="Details..." name="data" className="bg-gray-800 p-2 rounded-lg  md:m-2 my-2 w-[90%]" rows={4}  required/><br></br>
                       <button type="submit"  className="hover:cursor-pointer bg-gray-800 p-2 rounded-lg  md:m-2 my-2" disabled={isPending}>
         {isPending ? "Submitting..." : "Submit"}
       </button>
--- app/components/ProjectsSection.tsx
+++ app/components/ProjectsSection.tsx
@@ -1,9 +1,12 @@
-
+"use client";
 import Image from 'next/image';
 import ScrollReveal from './ScrollReveal';
 import {projectsList} from "../util/data";
 import ProjectItem from './ProjectItem';
+import { useState } from 'react';
 const ProjectsSection = () =>{
+    const [projectsNumber, setProjectsNumber] = useState(3);
+
     return (
         <section id="projects" className="relative py-28 space-y-24 ">
             {/* <div className="pointer-events-none absolute right-0 left-1/3 top-8 -z-10 h-[420px] opacity-40 blur-3xl" 
@@ -12,8 +15,15 @@ const ProjectsSection = () =>{
                                 <h2 className="text-3xl sm:text-3xl font-semibold  md:text-3xl font-normal tracking-right">
                 Projects</h2>
                <> {projectsList.map((listItem,position)=>{
+                if(position<projectsNumber){
                 return <ProjectItem key={listItem.name} name={listItem.name} position={position} description={listItem.description} image={listItem.image} scrollable={listItem.scrollable}/>
-               })}
+                }
+                else 
+                {
+                    return ;
+                }
+            })}
+               {projectsNumber-projectsList.length <0 && <div onClick={()=>setProjectsNumber(projectsNumber+3)} className="text-center self-center hover:cursor-pointer hover:text-[#880000]">See More</div>}
 </>
         </section>
     )
