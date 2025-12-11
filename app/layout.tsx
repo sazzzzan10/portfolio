0590881fae63b483ed1018a96f334a9e50f02c34
sazzzzan10 <sazzzzan10@gmail.com>
2025-12-11 21:45:07 +0545

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Pangolin } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+        {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID &&
+        process.env.NODE_ENV === "production" && (
+          <GoogleAnalytics 
+            gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID!}
+          />
+        )}
+
       <body
         className={` bg-[#0b0416] text-white min-h-screen ${pangolin.className} antialiased`}
       >
